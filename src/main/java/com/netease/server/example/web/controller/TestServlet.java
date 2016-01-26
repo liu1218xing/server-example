7507f56216e61355bb59af9661335bce0bf1b537
liuxing <liu1218xing@163.com>
2016-01-26 17:02:25 +0800

    -all

---
+++ src/main/java/com/netease/server/example/web/controller/TestServlet.java
@@ -0,0 +1,33 @@
+package com.netease.server.example.web.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6119506302329930242L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("service method");
+		super.service(req, resp);
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		System.out.println("doGet method");
+		PrintWriter pw = resp.getWriter();
+		pw.print("/hello");
+		pw.close();
+	}
+
+}
